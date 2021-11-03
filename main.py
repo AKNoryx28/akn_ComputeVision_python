9007069fc3912b3c5a09277ac2f328ccb60a7e99
AKNoryx28 <aknoryx28@yandex.com>
2021-11-03 10:24:43 +0700

    forgot some

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-from cv2 import FILLED, VideoCapture, imshow, waitKey, putText, FONT_HERSHEY_PLAIN, destroyAllWindows, imwrite
+from cv2 import FILLED, VideoCapture, eigenNonSymmetric, imshow, waitKey, putText, FONT_HERSHEY_PLAIN, destroyAllWindows, imwrite
 from time import time
 from keyboard import is_pressed
 # import modules.HandTracking as ht
 # import modules.PoseTracking as pt
 import modules.FaceTracking as ft
 
-cap = VideoCapture('TestVideos/1.mp4')
+cap = VideoCapture('TestVideos/2.mp4')
 # handDetector = ht.HandDetector()
 # poseDetector = pt.PoseDetection()
 faceDetector = ft.FaceDetector(0.7, 1)
 currentTime = 0
 pTime = 0
 
-while True:
+while cap.isOpened():
     ret, img = cap.read()
     if ret:
         # img = handDetector.findHands(img, True)
@@ -31,6 +31,9 @@ while True:
 
         putText(img, "fps: " + str(int(_FPS)), (15, 70), FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
         imshow("HandTracking", img)
+    else:
+        print("Video or webcam not found")
+        break
 
     if waitKey(1) & 0xFF == 27:
         break
